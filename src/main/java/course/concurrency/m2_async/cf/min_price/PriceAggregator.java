998ef993ff4fc10fb6d0bf0b5c4a7b0c2bd426b0
DYAKOV Vladimir <Vladimir.DYAKOV@raiffeisen.ru>
2022-08-11 00:05:05 +0300

    устранены замечания

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -3,6 +3,7 @@ package course.concurrency.m2_async.cf.min_price;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static java.lang.Double.NaN;
 
@@ -21,30 +22,18 @@ public class PriceAggregator {
     }
 
     public double getMinPrice(long itemId) {
-        long timeout = 1_000_000L - 1L;
-        List<CompletableFuture<Double>> completableFutures = new ArrayList<>(shopIds.size());
-        shopIds.forEach(
-                shopId -> {
-                    CompletableFuture<Double> completableFuture = CompletableFuture.supplyAsync(
-                            () -> priceRetriever.getPrice(itemId, shopId)
-                    ).completeOnTimeout(NaN, timeout, TimeUnit.MICROSECONDS).exceptionally(ex -> NaN);
-                    completableFutures.add(completableFuture);
-                }
-        );
-        OptionalDouble minPriceOpt = OptionalDouble.empty();
-        try {
-            minPriceOpt = CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0]))
-                    .thenApply(
-                            price -> completableFutures.parallelStream()
-                                    .filter(CompletableFuture::isDone)
-                                    .map(completableFuture -> completableFuture.getNow(NaN))
-                                    .filter(v -> !v.isNaN())
-                                    .mapToDouble(v -> v)
-                                    .min()
-                    ).get();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return minPriceOpt.orElse(NaN);
+        List<CompletableFuture<Double>> completableFutures = shopIds.stream()
+                .map(
+                        shopId -> CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(itemId, shopId))
+                                .completeOnTimeout(NaN, 999999, TimeUnit.MICROSECONDS)
+                                .exceptionally(ex -> NaN)
+                ).collect(Collectors.toList());
+        return CompletableFuture.allOf(completableFutures.toArray(CompletableFuture[]::new))
+                .thenApply(
+                        price -> completableFutures.parallelStream()
+                                .mapToDouble(CompletableFuture::join)
+                                .filter(value -> !Double.isNaN(value))
+                                .min()
+                ).join().orElse(NaN);
     }
 }
